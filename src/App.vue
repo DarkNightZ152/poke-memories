e66223dd0a37761426fdcf760f01baa8097e3a9a
Dark <thedarknight968@gmail.com>
2023-03-15 10:27:35 +0700

    first upload

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <main-screen
+      v-if="statusMatch === 'default'"
+      @onStart="onHandleBeforeStart($event)"
+    />
+    <interact-screen
+      v-if="statusMatch === 'match'"
+      :cardsContext="settings.cardsContext"
+      @onFinish="onGetResult"
+    />
+    <result-screen
+      v-if="statusMatch === 'result'"
+      :timer="timer"
+      @onStartAgain="statusMatch = 'default'"
+    />
+    <CopyRightScreen v-if="statusMatch === 'default'" />
+  </div>
+</template>
+
+<script>
+import MainScreen from "./components/MainScreen.vue";
+import InteractScreen from "./components/InteractScreen.vue";
+import ResultScreen from "./components/ResultScreen.vue";
+import { shuffled } from "./utils/array";
+import CopyRightScreen from "./components/CopyRightScreen.vue";
+export default {
+  name: "App",
+  components: {
+    MainScreen,
+    InteractScreen,
+    ResultScreen,
+    CopyRightScreen,
+  },
+  data() {
+    return {
+      settings: {
+        totalOfBlocks: 0,
+        cardsContext: [],
+        startedAt: null,
+      },
+      statusMatch: "default",
+      timer: 0,
+    };
+  },
+  methods: {
+    onHandleBeforeStart(config) {
+      console.log("running handle before start", config);
+      this.settings.totalOfBlocks = config.totalOfBlocks;
+
+      const firstCards = Array.from(
+        { length: this.settings.totalOfBlocks / 2 },
+        (_, i) => i + 1
+      );
+      const secondCards = [...firstCards];
+      const cards = [...firstCards, ...secondCards];
+      this.settings.cardsContext = shuffled(
+        shuffled(shuffled(shuffled(cards)))
+      );
+      this.settings.startedAt = new Date().getTime();
+
+      this.statusMatch = "match";
+    },
+    onGetResult() {
+      this.timer = new Date().getTime() - this.settings.startedAt;
+
+      this.statusMatch = "result";
+    },
+  },
+};
+</script>
